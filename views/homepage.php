b42056a8f8d4ce50879bb20c3a307a5ee09905e4
Horacio Santos Chi <529023962@qq.com>
2017-10-18 18:04:09 +0800

    v1.0

---
+++ views/homepage.php
@@ -0,0 +1,213 @@
+
+<!-- saved from url=(0043)http://www.feedmusic.com/views/homepage.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=GBK"></head><body><div id="homepage-page" class="page">
+    <div class="cineslider-container">
+        <div class="cineslider-wrapper">
+            <div id="homepage-introduction" class="cineslider-slide">
+                <div class="border" data-position="top"></div>
+                <div class="border" data-position="right"></div>
+                <div class="border" data-position="left"></div>
+                <div class="border" data-position="bottom"></div>
+                <div class="intro-paragraph">
+                    <p>Feed is an intelligent property rights and payments platform, using intelligent software and digital security that goes well beyond 'military-grade' to give users true ownership of their data and IP.</p>
+                    <p>Feed facilitates trusted exchanges of users' progressively-perfecting data assets with businesses, researchers, and governments in a <b>trusted</b>, audited, and independently verifiable manner; on their own terms and conditions.</p>
+                </div>
+                <div class="videobg-container"></div>
+                <div class="starwars-container">
+                    <div class="starwars-wrapper">
+                        <p>When you want something, </p>
+                        <p>all the universe conspires </p>
+                        <p>in helping you to achieve it.</p>
+                        <p>
+                            <strong>
+                                <i><small>Paulo Coelho</small></i>
+                            </strong>
+                        </p>
+
+                        <p><br><br></p>
+                        <p><br><br></p>
+                        <p>Feed is that conspiracy:</p>
+                        <p>the conspiracy of trust.</p>
+                        <p><br><br></p>
+                        <p><br><br></p>
+
+                        <p>Trust is the single</p>
+                        <p>most important ingredient </p>
+                        <p>missing from digital relationships.</p>
+                        <p><br><br></p>
+                        <p><br><br></p>
+
+                        <p>Boston Consulting Group </p>
+                        <p>and the World Economic Forum </p>
+                        <p>forecast the digital economy </p>
+                        <p>to be worth between </p>
+                        <p><strong>1.5 and 2.5 trillion dollars</strong></p>
+                        <p><strong>by 2016.</strong></p>
+                        <p><br><br></p>
+                        <p><br><br></p>
+
+                        <p>The difference </p>
+                        <p>between those numbers </p>
+                        <p>is <a href="http://www3.weforum.org/docs/WEF_IT_RethinkingPersonalData_Report_2012.pdf" target="_blank">trust</a>.</p>
+                        <p><br><br></p>
+                        <p><br><br></p>
+
+                        <p style="white-space: normal">
+                           Feed is a digital mechanism of <strong>trust</strong>
+                        </p>
+                    </div>
+                </div>
+
+                <a href="http://www.feedmusic.com/views/homepage.php#" class="scroll-down">
+                    <i class="icon"></i>
+                    <span>scroll down</span>
+                </a>
+            </div>
+
+            <div id="homepage-technology" class="cineslider-slide">
+                <div class="background"></div>
+                <div class="video-popup">
+                    <div class="video-popup-wrapper">
+                        <div class="content">
+                            <a href="http://www.feedmusic.com/views/homepage.php#" class="video-popup-play">
+                                <i class="fa fa-play"></i>
+                            </a>
+                            <p>Introducing Feed</p>
+                        </div>
+                    </div>
+
+                    <div class="video-popup-overlay">
+                        <video class="video-js vjs-default-skin" controls="" preload="none" width="100%" height="100%" data-setup="{&quot;example_option&quot;:true}">
+                             <source src="videos/introducing-feed.mp4" type="video/mp4">
+                             <source src="videos/introducing-feed.webm" type="video/webm">
+                            <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+                        </video>
+                    </div>
+
+                    <a href="http://www.feedmusic.com/views/homepage.php#" class="video-popup-close"></a>
+                </div>
+                <a href="http://www.feedmusic.com/views/homepage.php#" class="scroll-down">
+                    <i class="icon"></i>
+                    <span>scroll down</span>
+                </a>
+            </div>
+
+            <div id="homepage-tech-spotlight" class="cineslider-slide" data-menu-invert="true">
+                <div class="border" data-position="top"></div>
+                <div class="border" data-position="right"></div>
+                <div class="border" data-position="left"></div>
+                <div class="border" data-position="bottom"></div>
+
+                <h1 class="title">intelligent digital instruments</h1>
+
+                <div class="swiper-container toggle">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide" data-name="human" data-start="16" data-end="37">
+                            <div class="swiper-slide-wrapper">
+                                <p data-swiper-parallax-y="-100" data-swiper-parallax-duration="1200">Data is recorded in provenance: meaning your personal data asset is permanent, immutable, and continuously growing in size and value. Automate the discovery and exploration of causal and correlative relationships that structure your personal provenance graph. Control what others can see and what they can’t. Your provenance graph is a rich, living asset that will be forever treasured and alive for you and your heirs.</p>
+                                <div class="p-line" data-swiper-parallax-x="100%" data-swiper-parallax-duration="1200"></div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide" data-name="brain" data-start="38" data-end="61">
+                            <div class="swiper-slide-wrapper">
+                                <p data-swiper-parallax-y="-100" data-swiper-parallax-duration="1200">Feed’s intelligent agents are cognitively aware of the changes in state of people, places, events, objects, and states of affairs.</p>
+                                <div class="p-line" data-swiper-parallax-x="100%" data-swiper-parallax-duration="1200"></div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide" data-name="puzzle" data-start="62" data-end="94">
+                            <div class="swiper-slide-wrapper">
+                                <p data-swiper-parallax-y="-100" data-swiper-parallax-duration="1200">When you can see the origin of data, and you have real-time access to changes in the state of that data, you have the most powerful marketing effectiveness measurement on the planet.</p>
+                                <div class="p-line" data-swiper-parallax-x="100%" data-swiper-parallax-duration="1200"></div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide" data-name="puzzle-group" data-start="95" data-end="121">
+                            <div class="swiper-slide-wrapper">
+                                <p data-swiper-parallax-y="-100" data-swiper-parallax-duration="1200">Which means real-time and real-state insight, which means higher ROI through verifiably improved economic effectiveness of online advertising and consumer services.</p>
+                                <div class="p-line" data-swiper-parallax-x="100%" data-swiper-parallax-duration="1200"></div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide" data-name="lightbulb" data-start="122" data-end="149">
+                            <div class="swiper-slide-wrapper">
+                                <p data-swiper-parallax-y="-100" data-swiper-parallax-duration="1200">Continuous, automatic, and progressively-perfecting discovery: the most powerful tool for discovering that next gig, relationship, customer, &amp; opportunity.</p>
+                                <div class="p-line" data-swiper-parallax-x="100%" data-swiper-parallax-duration="1200"></div>
+                            </div>
+                        </div>
+
+                        <div class="swiper-slide" data-name="human" data-start="150" data-end="179">
+                            <div class="swiper-slide-wrapper">
+                                <p data-swiper-parallax-y="-100" data-swiper-parallax-duration="1200">Trust. Ownership. Discovery. With Feed, everything that is yours, is now actually yours.</p>
+                                <div class="p-line" data-swiper-parallax-x="100%" data-swiper-parallax-duration="1200"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <a href="http://www.feedmusic.com/views/homepage.php#" class="scroll-down invert">
+                    <i class="icon"></i>
+                    <span>scroll down</span>
+                </a>
+            </div>
+
+            <div id="homepage-why-music" class="cineslider-slide">
+                <div class="background"></div>
+                <div class="video-popup">
+                    <div class="video-popup-wrapper">
+                        <div class="content">
+                            <a href="http://www.feedmusic.com/views/homepage.php#" class="video-popup-play">
+                                <i class="fa fa-play"></i>
+                            </a>
+                            <p>Why start with music?</p>
+                        </div>
+                    </div>
+
+                    <div class="video-popup-overlay">
+                        <video class="video-js vjs-default-skin" controls="" preload="none" width="100%" height="100%" data-setup="{&quot;example_option&quot;:true}">
+                             <source src="videos/why-music.mp4" type="video/mp4">
+                             <source src="videos/why-music.webm" type="video/webm">
+                            <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+                        </video>
+                    </div>
+
+                    <a href="http://www.feedmusic.com/views/homepage.php#" class="video-popup-close"></a>
+                </div>
+                <a href="http://www.feedmusic.com/views/homepage.php#" class="scroll-down">
+                    <i class="icon"></i>
+                    <span>scroll down</span>
+                </a>
+            </div>
+
+            <div id="homepage-feed-app" class="cineslider-slide">
+                <div class="border" data-position="top"></div>
+                <div class="border" data-position="right"></div>
+                <div class="border" data-position="left"></div>
+                <div class="border" data-position="bottom"></div>
+
+                <div class="splitter">
+                    <div class="splitter-item">
+                        <div class="vertical-container">
+                            <div class="vertical-content">
+                                <a href="http://www.feedmusic.com/views/homepage.php#/feed-app" class="splitter-button">
+                                    learn <br> more
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="splitter-item">
+                        <div class="vertical-container">
+                            <div class="vertical-content">
+                                <a href="http://www.feedmusic.com/views/homepage.php#/schedule-meeting" class="splitter-button">
+                                    schedule <br>a meeting
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body></html>
